e176bd6f281d632f43d0af87054feee30c348fd1
Somaya <somaya224@gmail.com>
2020-09-29 15:25:55 -0400

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: 'Client-ID WSDJpKy9_UX3V7WOtQonSOSj1xjpMRHOG3hcrdotze8'
+    Authorization: 'Client-ID the key goes here'
   }
 });
